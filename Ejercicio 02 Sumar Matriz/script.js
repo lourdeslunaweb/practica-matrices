1c5d4721e81dc73561b6042bba48ad09c71e135c
Lourdes <lourdeslunaweb@gmail.com>
2021-06-18 15:23:20 -0300

    Ejercicio 2 realizado

--- Ejercicio 02 Sumar Matriz/script.js
+++ Ejercicio 02 Sumar Matriz/script.js
@@ -6,3 +6,19 @@
 //   [2, 7, 1],
 //   [8, 10],
 // ]) // 37
+
+const sumar = (matriz) => {
+    let resultado = 0
+    for (let i = 0; i < matriz.length; i++) {
+        for (let j=0; j < matriz[i].length; j++) {
+            resultado += matriz[i][j]
+        }
+    }
+    return resultado
+}
+
+console.log(sumar([
+    [4, 5],
+    [2, 7, 1],
+    [8, 10],
+]));
