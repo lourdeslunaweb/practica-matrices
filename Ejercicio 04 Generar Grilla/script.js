c447915acb09fb989edeb5cfe954e6558af4b949
Lourdes <lourdeslunaweb@gmail.com>
2021-06-18 21:01:26 -0300

    Ejercicio 4 realizado

--- Ejercicio 04 Generar Grilla/script.js
+++ Ejercicio 04 Generar Grilla/script.js
@@ -18,3 +18,20 @@
 //     ['b', 'a', 'b']
 //   ]
 // */
+
+const generarGrilla = (filas, columnas, items) => {
+    let grilla = []
+    for (let i = 0; i < filas; i++) {
+        let fila = []
+        for (let j = 0; j < columnas; j++) {
+            fila.push(items[Math.floor(Math.random() * items.length)])
+        }
+        grilla.push(fila)
+    }
+    return grilla
+}
+
+console.log(generarGrilla(2, 3, [1, 2]));
+
+
+console.log(generarGrilla(3, 3, ['a', 'b', 'c']));
